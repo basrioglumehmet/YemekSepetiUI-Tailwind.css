7b2774045b2015c5a0630d8a42b3af915457f3ed
Basrioglu Mehmet <166669195+basrioglumehmet@users.noreply.github.com>
2024-09-08 16:44:53 +0300

    Create tailwind.config.js
    All Definitions

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      // ...
+      colors: {
+        interactionHighlight: '#f7f7f7',
+        secondary:{
+          main:"#333333"
+        }
+      },
+    },
+  },
+  plugins: [],
+}
